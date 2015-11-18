e3a44eebb3970c5bcce53809071e108ab35697b1
Alejandro Aranda Gomez <alejandro.aranda.contractor@dev.bbva.com>
2015-11-18 13:32:09 +0100

    Initial commit

---
+++ src/main/java/com/test/config/ObjectifyConfig.java
@@ -0,0 +1,41 @@
+package com.test.config;
+
+import com.googlecode.objectify.ObjectifyService;
+import com.googlecode.objectify.impl.translate.opt.joda.DateTimeZoneTranslatorFactory;
+import com.googlecode.objectify.impl.translate.opt.joda.LocalDateTimeTranslatorFactory;
+import com.googlecode.objectify.impl.translate.opt.joda.LocalDateTranslatorFactory;
+import com.googlecode.objectify.impl.translate.opt.joda.ReadableInstantTranslatorFactory;
+import com.test.data.TestBean;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Class to load bean to persistent into DataStore.
+ */
+public class ObjectifyConfig implements ServletContextListener {
+
+    static {
+        // Add translators for Joda objects
+
+        ObjectifyService.factory().getTranslators()
+                .add(new ReadableInstantTranslatorFactory());
+        ObjectifyService.factory().getTranslators()
+                .add(new LocalDateTranslatorFactory());
+        ObjectifyService.factory().getTranslators()
+                .add(new LocalDateTimeTranslatorFactory());
+        ObjectifyService.factory().getTranslators()
+                .add(new DateTimeZoneTranslatorFactory());
+
+        // TODO: Add and register each data bean here
+        ObjectifyService.register(TestBean.class);
+    }
+
+    @Override
+    public void contextInitialized(final ServletContextEvent sce) {
+    }
+
+    @Override
+    public void contextDestroyed(final ServletContextEvent sce) {
+    }
+}
